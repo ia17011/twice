388d03dd1a932edca3bc0847e06516c0011aa567
ia17011 <cfoyvlird@gmail.com>
2019-02-17 12:22:15 +0900

    working correctly but, including RT has significant favorite. so the resutl makes me sad.

--- main.py
+++ main.py
@@ -16,20 +16,12 @@ class TweetCollector:
     __user_name = ""
     __tweet_data = []
 
+    __fav_sum = 0
+    __avg = 0
+
+
     def __init__(self, user_name):
         self.__user_name = user_name
-
-        row = [
-            "ツイートID",
-            "名前",
-            "ユーザー名",
-            "投稿日",
-            "本文",
-            "リツイート数",
-            "いいね数"
-        ]
-
-        self.__tweet_data.append(row)
 
     def collectTweet(self):
         self.nextTweet(0)
@@ -51,16 +43,12 @@ class TweetCollector:
 
         items = soup.select(".js-stream-item")
 
-        sum = 0
-
         for item in items:
             row = []
-            #row.append(item.get("data-item-id"))
             row.append(item.select_one(".fullname").get_text().encode("cp932", "ignore").decode("cp932"))
             row.append(item.select_one(".username").get_text())
             row.append(item.select_one("._timestamp").get_text())
             row.append(item.select_one(".js-tweet-text-container").get_text().strip().encode("cp932", "ignore").decode("cp932"))
-            #row.append(item.select(".ProfileTweet-actionCountForPresentation")[1].get_text())
             if item.select(".ProfileTweet-actionCountForPresentation")[1].get_text():
                 rt = item.select(".ProfileTweet-actionCountForPresentation")[1].get_text()
                 rt = rt.replace(',', '')
@@ -75,6 +63,7 @@ class TweetCollector:
             else:
                 row.append(0)
 
+            self.__fav_sum += row[5]
             self.__tweet_data.append(row)
 
         print(str(max_position) + "← このツイートIDのヤツは取得完了したで")
@@ -83,17 +72,31 @@ class TweetCollector:
         if res.json()["min_position"] is not None:
             self.nextTweet(res.json()["min_position"])
 
+    def just_high_avg(self):
+        self.__avg = round(self.__fav_sum / len(self.__tweet_data))
+        print(self.__avg)
+        for item in self.__tweet_data:
+            try:
+                if self.__avg >= item[5]:
+                    item.clear()
+                else:
+                    continue
+            except:
+                pass
+
     def writeCSV(self):
         today = datetime.now().strftime("%Y%m%d%H%M")
         with open(self.__user_name + "-" + today + ".csv", "w") as f:
             writer = csv.writer(f, lineterminator='\n')
             writer.writerows(self.__tweet_data)
 
+
 print('このCLIツールでは、入力されたアカウントのお気に入り平均以上のツイートをCSVファイルに出力するで')
 inp_un = input('取得したいアカウントのIDを入力してな (@は除く) >> ')
 
 twc = TweetCollector(inp_un)
 twc.collectTweet()
+twc.just_high_avg()
 twc.writeCSV()
 
 
